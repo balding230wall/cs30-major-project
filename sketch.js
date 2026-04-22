782679c98e60dabf572fb699200b3d52a96b0e8e
balding230wall <chuyanwang595@gmail.com>
2026-04-22 11:00:57 -0600

    uploaded picture of the main playable character

--- sketch.js
+++ sketch.js
@@ -5,12 +5,20 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let redBirdImg;
+
+function preload(){
+  redBirdImg = loadImage("redBird.png");
+}
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
 }
 
 function draw() {
   background(220);
-  circle(mouseX, mouseY, 50);
+  image(redBirdImg, mouseX, mouseY, redBirdImg.width * 0.2, redBirdImg.height * 0.2);
+
 }
