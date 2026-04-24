f391e232872ea88b399c4f817213a7e1fad72086
balding230wall <chuyanwang595@gmail.com>
2026-04-24 11:40:59 -0600

    loaded villain images into the game

--- sketch.js
+++ sketch.js
@@ -7,10 +7,16 @@
 
 let redBirdImg;
 let parkBackgroundImg;
+let clownImg;
+let crazyClownImg;
+let amuesmentRobotImg;
 
 function preload(){
   redBirdImg = loadImage("redBird.png");
   parkBackgroundImg = loadImage("amuesmentPark.png");
+  clownImg = loadImage("amuesmentclown.png");
+  crazyClownImg = loadImage("amuesmentcrazyclown.png");
+  amuesmentRobotImg = loadImage("amuesmentrobot.png");
 
 }
 
@@ -24,5 +30,8 @@ function draw() {
   background(220);
   image(parkBackgroundImg, windowWidth/2, windowHeight/2, windowWidth, windowHeight);
   image(redBirdImg, mouseX, mouseY, redBirdImg.width * 0.2, redBirdImg.height * 0.2);
+  image(clownImg, windowWidth - 100, windowHeight - 300, clownImg.width * 0.2, clownImg.height * 0.2);
+  image(crazyClownImg, windowWidth - 100, windowHeight - 200, crazyClownImg.width * 0.2, crazyClownImg.height * 0.2);
+  image(amuesmentRobotImg, windowWidth - 100, windowHeight - 100, amuesmentRobotImg.width * 0.2, amuesmentRobotImg.height * 0.2);
 
 }
