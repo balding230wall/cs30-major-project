da154e62e54ab00beae13c16e7464ec70c28caaa
balding230wall <chuyanwang595@gmail.com>
2026-04-30 11:01:41 -0600

    adjusted all code previously to oop style

--- sketch.js
+++ sketch.js
@@ -15,6 +15,8 @@ let rocketProjectileImg;
 let clownProjectileImg;
 let crazyClownProjectileImg;
 
+let birdShip;
+
 function preload(){
   redBirdImg = loadImage("redBird.png");
   parkBackgroundImg = loadImage("amusementparkbackground.png");
@@ -28,60 +30,73 @@ function preload(){
 
 }
 
-class Projectiles{
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.dx = -5;
-  }
-
-  display(){
-    image(rocketProjectileImg, amusementRobotImg.x - amusementRobotImg.width * 0.1, amusementRobotImg.y, rocketProjectileImg.width * 0.1, rocketProjectileImg.height * 0.1);
-    image(clownProjectileImg, clownImg.x - clownImg.width * 0.1, clownImg.y, clownProjectileImg.width * 0.1, clownProjectileImg.height * 0.1);
-    image(crazyClownProjectileImg, crazyClownImg.x - crazyClownImg.width * 0.1, crazyClownImg.y, crazyClownProjectileImg.width * 0.1, crazyClownProjectileImg.height * 0.1);
-  }
-
-  update(){
-    this.x += this.dx;
-  }
-}
-
-class Characters{
-  constructor(x, y){
-    this.x = x;
-    this.y = y;
-    this.dx = -5;
-    this.dy = random(-2, 2);
-  }
-  
-  display(){
-    image(clownImg, windowWidth - 100, windowHeight - 300, clownImg.width * 0.2, clownImg.height * 0.2);
-    image(crazyClownImg, windowWidth - 100, windowHeight - 200, crazyClownImg.width * 0.2, crazyClownImg.height * 0.2);
-    image(amusementRobotImg, windowWidth - 100, windowHeight - 100, amusementRobotImg.width * 0.2, amusementRobotImg.height * 0.2);
-  }
-
-  update(){
-    this.x += this.dx;
-    this.y += this.dy;
-  }
-}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
+  birdShip = new Character(mouseX, mouseY, redBirdImg);
 }
 
 function draw() {
   background(220);
   createImages();
+  birdShip.display();
 }
 
 function createImages() {
   image(parkBackgroundImg, windowWidth/2, windowHeight/2, windowWidth, windowHeight);
-  image(redBirdImg, mouseX, mouseY, redBirdImg.width * 0.2, redBirdImg.height * 0.2);
-  image(mainProjectileImg, mouseX + redBirdImg.width * 0.1, mouseY, mainProjectileImg.width * 0.1, mainProjectileImg.height * 0.1);
 }
 
 function mouseClicked(){
-  image(mainProjectileImg, mouseX + redBirdImg.width * 0.1, mouseY, mainProjectileImg.width * 0.1, mainProjectileImg.height * 0.1);
+  birdShip.fire();
+}
+
+class Character{
+  constructor(x, y, theImage){
+    this.x = x;
+    this.y = y;
+    this.image = theImage;
+    this.projectileArray = [];
+  }
+  
+  display(){
+    image(this.image, this.x, this.y);
+
+    for(let projectile of this.projectileArray){
+      if (projectile.isOnScreen){
+        projectile.update();
+        projectile.display();
+      }
+      else {
+        this.projectileArray.splice(this.projectileArray.indexOf(projectile), 1);
+      }
+    }
+  }
+
+  fire(){
+    let thatProjectile = new Projectiles(this.x, this.y, 5, 0, mainProjectileImg);
+    this.projectileArray.push(thatProjectile);
+  }
+}
+
+class Projectiles{
+  constructor(x, y, dx, dy, theImage) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.image = theImage;
+  }
+
+  display(){
+    image(this.image, this.x, this.y);
+  }
+
+  update(){
+    this.x += this.dx;
+  }
+
+  isOnScreen(){
+    return this.x > 0;
+  }
 }
